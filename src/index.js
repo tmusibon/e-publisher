fe77c5a38fb684c2d06f9c8288b3b309eb7ea562
Thierry Musibon <thierrymusibon@Thierrys-MacBook-Pro.local>
2022-03-24 16:00:37 -0400

    adding files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./assets/medium.css";
+import { Provider } from "react-redux";
+import { Switch, Route } from "react-router-dom";
+import { ConnectedRouter } from "react-router-redux";
+import App from "./App.js";
+import registerServiceWorker from "./registerServiceWorker";
+import { store, history } from "./redux/store";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Switch>
+        <Route path="/" component={App} />
+      </Switch>
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+registerServiceWorker();
